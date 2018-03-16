2a7fe4d3ea99de7d55df92829499d7b500f981dc
hwq <532353303@qq.com>
2018-03-16 15:36:28 +0800

    编译时动态生成JAVA代码！ Apt注解解释工具，apt Annotation process tool

---
+++ app/src/main/java/com/uascent/tx/sdk/jxtxwifisdk/test/AptTestActivity.java
@@ -0,0 +1,38 @@
+package com.uascent.tx.sdk.jxtxwifisdk.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.uascent.tx.sdk.jxtxwifisdk.R;
+
+import jxtxwifisdk.sdk.tx.uascent.com.apt_lib.AutoCreate;
+
+/**
+ * 流程：
+ * 1.创建apt注解java-lib
+ * -------------@Target(ElementType.TYPE)
+ * -------------@Retention(RetentionPolicy.RUNTIME)
+ * 2.创建apt处理器java-lib
+ * 3.处理器lib引用库：
+ * -------------implementation 'com.squareup:javapoet:1.9.0'
+ * -------------implementation 'com.google.auto.service:auto-service:1.0-rc3'
+ * 4.处理器lib创建class
+ * -------添加注解：“@AutoService(Processor.class)”
+ * -------继承：AbstractProcessor
+ * -------在process中写下你要生成代码的逻辑
+ * 5.在App中需要生成程序的页面添加：注解
+ * <p>
+ * 注意点：
+ * 1.Android3.0跟目录不需要添加APT依赖
+ * 2.process文件中不能有任何注释！！
+ * 3.创建的lib必须为Java Lib
+ */
+@AutoCreate
+public class AptTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apt_test);
+    }
+}
---
+++ apt_process/src/main/java/jxtxwifisdk/sdk/tx/uascent/com/apt_process/AptProcess2.java
@@ -0,0 +1,84 @@
+package jxtxwifisdk.sdk.tx.uascent.com.apt_process;
+
+
+import com.google.auto.service.AutoService;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+
+import jxtxwifisdk.sdk.tx.uascent.com.apt_lib.AutoCreate;
+
+@AutoService(Processor.class)
+public class AptProcess2 extends AbstractProcessor {
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(AutoCreate.class.getCanonicalName());
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment
+            roundEnv) {
+
+        FieldSpec name = FieldSpec.builder(String.class, "name", Modifier.PRIVATE)
+                .build();
+        FieldSpec age = FieldSpec.builder(int.class, "age", Modifier.PRIVATE).build();
+
+        MethodSpec methodConstructor = MethodSpec.methodBuilder("User")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(String.class, "name")
+                .addParameter(int.class, "age")
+                .addStatement("this.age = age")
+                .addStatement("this.name = name")
+                .build();
+
+        MethodSpec methodGetName = MethodSpec.methodBuilder("getName")
+                .addModifiers(Modifier.PUBLIC)
+                .returns(String.class)
+                .addStatement("return name")
+                .build();
+
+        MethodSpec methodSetName = MethodSpec.methodBuilder("setName")
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(String.class, "name")
+                .returns(void.class)
+                .addStatement("this.name = name")
+                .build();
+
+        MethodSpec methodPrintName = MethodSpec.methodBuilder("printName")
+                .addModifiers(Modifier.PUBLIC)
+                .addStatement("$T.out.println(name)", System.class)
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("User")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(methodConstructor)
+                .addMethod(methodGetName)
+                .addMethod(methodSetName)
+                .addMethod(methodPrintName)
+                .addField(name)
+                .addField(age)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.uascent.tx.sdk.jxtxwifisdk", helloWorld)
+                .build();
+
+        try {
+            javaFile.writeTo(processingEnv.getFiler());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
